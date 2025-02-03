ecb06e3965e31ea2c2a2a7904b29fee123d43905
Phoenix <134625903+Nix-phoenix@users.noreply.github.com>
2025-02-03 16:23:40 +0700

    Delete CompanyEmployee/CompanyEmployee directory

--- CompanyEmployee/CompanyEmployee/src/main/java/CompanyEmployee/Repository/EmployeeRepostitory.java
+++
@@ -1,9 +0,0 @@
-package CompanyEmployee.Repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import CompanyEmployee.Model.Employee;
-
-public interface EmployeeRepostitory extends JpaRepository<Employee, Integer> {
-    
-}
